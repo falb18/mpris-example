4406bf2ab68f4c0bba891e4f357df05876063fc5
falb18 <arturo.lopezbo@gmail.com>
2023-06-28 17:56:39 -0600

    mpris-track-info.py: Add script to get metadata from Spotify
    The script retreives the metadata from the song which is currently played by
    Spotify. It uses the D-Bus MPRIS2 to communicate with Spotify's desktop
    application.

---
+++ mpris-track-info.py
@@ -0,0 +1,19 @@
+import dbus
+from mpris2 import utils, player, interfaces
+
+# Metadata fields keys
+track_title = 'xesam:title'
+track_album = 'xesam:album'
+
+print(f"MPRIS2 bus name: {interfaces.Interfaces.MEDIA_PLAYER}")
+print(f"MPRIS2 object path: {interfaces.Interfaces.OBJECT_PATH}")
+
+uri = interfaces.Interfaces.MEDIA_PLAYER + ".spotify"
+print(f"Spotify bus name: {uri}")
+
+spotify_player = player.Player(dbus_interface_info={'dbus_uri': uri})
+print(spotify_player.PlaybackStatus)
+if spotify_player.PlaybackStatus == "Playing" or spotify_player.PlaybackStatus == "Paused":
+    print("\nCurrent track information:")
+    print("\tTitle: " + spotify_player.Metadata[track_title])
+    print("\tAlbum: " + spotify_player.Metadata[track_album])

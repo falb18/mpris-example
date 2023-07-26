8c8662b2a08411a6fbde609fe72313b73668f5a8
falb18 <arturo.lopezbo@gmail.com>
2023-07-25 19:37:23 -0600

    mpris-track-info.py: Add empty lines to the console output

--- mpris-track-info.py
+++ mpris-track-info.py
@@ -5,6 +5,7 @@ from mpris2 import utils, player, interfaces
 track_title = 'xesam:title'
 track_album = 'xesam:album'
 
+print()
 print(f"MPRIS2 bus name: {interfaces.Interfaces.MEDIA_PLAYER}")
 print(f"MPRIS2 object path: {interfaces.Interfaces.OBJECT_PATH}")
 
@@ -12,8 +13,10 @@ uri = interfaces.Interfaces.MEDIA_PLAYER + ".spotify"
 print(f"Spotify bus name: {uri}")
 
 spotify_player = player.Player(dbus_interface_info={'dbus_uri': uri})
-print(spotify_player.PlaybackStatus)
+print(f"Spotify playback status: {spotify_player.PlaybackStatus}")
+
 if spotify_player.PlaybackStatus == "Playing" or spotify_player.PlaybackStatus == "Paused":
     print("\nCurrent track information:")
     print("\tTitle: " + spotify_player.Metadata[track_title])
     print("\tAlbum: " + spotify_player.Metadata[track_album])
+    print()

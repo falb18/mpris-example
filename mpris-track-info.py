c2362f2997ace906ceeaa774badbe248bffde862
falb18 <arturo.lopezbo@gmail.com>
2024-04-11 19:45:12 -0600

    mpris-track-info.py: Add error handler when Spotify player is not running

--- mpris-track-info.py
+++ mpris-track-info.py
@@ -1,4 +1,6 @@
 import dbus
+import sys
+from dbus.exceptions import DBusException
 from mpris2.mpris2 import player, interfaces
 from mpris2.mpris2.types import Metadata_Map
 
@@ -9,11 +11,17 @@ print(f"MPRIS2 object path: {interfaces.Interfaces.OBJECT_PATH}")
 uri = interfaces.Interfaces.MEDIA_PLAYER + ".spotify"
 print(f"Spotify bus name: {uri}")
 
-spotify_player = player.Player(dbus_interface_info={'dbus_uri': uri})
-print(f"Spotify playback status: {spotify_player.PlaybackStatus}")
+try:
+    spotify_player = player.Player(dbus_interface_info={'dbus_uri': uri})
+except DBusException as dbus_exception:
+    # Catch the exception when the Spotify player is not running
+    print(f"DBusException: {str(dbus_exception)}")
+    sys.exit(1)
+else:
+    print(f"Spotify playback status: {spotify_player.PlaybackStatus}")
 
-if spotify_player.PlaybackStatus == "Playing" or spotify_player.PlaybackStatus == "Paused":
-    print("\nCurrent track information:")
-    print("\tTitle: " + spotify_player.Metadata[Metadata_Map.TITLE])
-    print("\tAlbum: " + spotify_player.Metadata[Metadata_Map.ALBUM])
-    print()
+    if spotify_player.PlaybackStatus == "Playing" or spotify_player.PlaybackStatus == "Paused":
+        print("\nCurrent track information:")
+        print("\tTitle: " + spotify_player.Metadata[Metadata_Map.TITLE])
+        print("\tAlbum: " + spotify_player.Metadata[Metadata_Map.ALBUM])
+        print()
